46110f769f422b64ae3fdae297d1f7d5fa398e78
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-13 18:48:10 +0200

    Declare the Uint64 type

--- src/helios.ads
+++ src/helios.ads
@@ -15,5 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Interfaces;
 package Helios is
+   subtype Uint64 is Interfaces.Unsigned_64;
 end Helios;
