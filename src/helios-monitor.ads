f4e6d377116b3dbfbb9b50c100cbed9be6b5b395
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-14 18:14:03 +0200

    Refactor the Monitor package

--- src/helios-monitor.ads
+++ src/helios-monitor.ads
@@ -18,67 +18,26 @@
 with Ada.Calendar;
 with Ada.Real_Time;
 with Ada.Finalization;
+with Helios.Schemas;
+with Helios.Datas;
 package Helios.Monitor is
 
-   type Value_Type is (V_INTEGER);
-
-   type Value_Index is new Natural;
-   subtype Value_Array_Index is Value_Index range 1 .. Value_Index'Last;
-
-   type Definition_Type;
-   type Definition_Type_Access is access all Definition_Type'Class;
-
-   type Definition_Type (Len : Natural) is tagged record
-      Kind    : Value_Type;
-      Index   : Value_Index := 0;
-      Parent  : Definition_Type_Access;
-      Next    : Definition_Type_Access;
-      Child   : Definition_Type_Access;
-      Sibling : Definition_Type_Access;
-      Name    : String (1 .. Len);
-   end record;
-
-   type Value_Array is array (Value_Array_Index range <>) of Uint64;
-   type Value_Array_Access is access all Value_Array;
-
-   type Snapshot_Type is limited record
-      Time          : Ada.Calendar.Time;
-      Start_Time    : Ada.Real_Time.Time;
-      End_Time      : Ada.Real_Time.Time;
-      Values        : Value_Array_Access;
-   end record;
-
-   --  Get the root definition tree.
-   function Get_Root return Definition_Type_Access;
-
-   --  Initialize the snapshot.
-   procedure Initialize (Data : in out Snapshot_Type);
-
-   --  Set the value in the snapshot.
-   procedure Set_Value (Into  : in out Snapshot_Type;
-                        Def   : in Definition_Type_Access;
-                        Value : in Uint64);
-
    type Agent_Type is new Ada.Finalization.Limited_Controlled with record
-      Node : Definition_Type_Access;
+      Node : Schemas.Definition_Type_Access;
    end record;
 
    --  Create a new definition with the given name.
    function Create_Definition (Agent : in Agent_Type;
-                               Name  : in String) return Definition_Type_Access;
+                               Name  : in String) return Schemas.Definition_Type_Access;
 
    --  Add a definition to the agent.
    procedure Add_Definition (Agent : in out Agent_Type;
-                             Def   : in Definition_Type_Access);
-
-   --  Add a new definition node to the definition.
-   function Create_Definition (Into : in Definition_Type_Access;
-                             Name : in String) return Definition_Type_Access;
+                             Def   : in Schemas.Definition_Type_Access);
 
    --  Find a child definition with the given name.
    --  Returns null if there is no such definition.
    function Find_Definition (Agent : in Agent_Type;
-                             Name  : in String) return Definition_Type_Access;
+                             Name  : in String) return Schemas.Definition_Type_Access;
 
    --  Register the agent.
    procedure Register (Agent : in out Agent_Type'Class;
@@ -89,6 +48,6 @@ package Helios.Monitor is
 
    --  Collect the values in the snapshot.
    procedure Collect (Agent  : in out Agent_Type;
-                      Values : in out Snapshot_Type) is null;
+                      Values : in out Datas.Snapshot_Type) is null;
 
 end Helios.Monitor;
