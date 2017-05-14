891571908bb9a7f8d0459dd139db40d8f92a474b
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-14 18:12:41 +0200

    Moved operations from Monitor package

---
+++ src/helios-schemas.adb
@@ -0,0 +1,90 @@
+-----------------------------------------------------------------------
+--  helios-schemas -- Helios schemas
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package body Helios.Schemas is
+
+   Root          : aliased Definition_Type (Len => 0);
+   Current_Index : Value_Index := 0;
+
+   --  ------------------------------
+   --  Get the root definition tree.
+   --  ------------------------------
+   function Get_Root return Definition_Type_Access is
+   begin
+      return Root'Access;
+   end Get_Root;
+
+   --  ------------------------------
+   --  Get the number of definition values.
+   --  ------------------------------
+   function Get_Count return Value_Index is
+   begin
+      return Current_Index;
+   end Get_Count;
+
+   --  ------------------------------
+   --  Add a new definition node to the definition.
+   --  ------------------------------
+   function Create_Definition (Into : in Definition_Type_Access;
+                               Name : in String;
+                               Kind : in Value_Type := V_INTEGER) return Definition_Type_Access is
+      Result : constant Definition_Type_Access := new Definition_Type (Len => Name'Length);
+   begin
+      Result.Kind := Kind;
+      if Kind /= V_NONE then
+         Current_Index := Current_Index + 1;
+         Result.Index  := Current_Index;
+      end if;
+      Result.Name   := Name;
+      Result.Parent := Into;
+      Result.Next   := Into.Child;
+      Into.Child    := Result;
+      return Result;
+   end Create_Definition;
+
+   --  ------------------------------
+   --  Add a definition to the agent.
+   --  ------------------------------
+   procedure Add_Definition (Into : in Definition_Type_Access;
+                             Def  : in Definition_Type_Access) is
+   begin
+      Current_Index := Current_Index + 1;
+      Def.Parent := Into;
+      Def.Next := Into.Child;
+      Def.Index := Current_Index;
+      Into.Child := Def;
+   end Add_Definition;
+
+   --  ------------------------------
+   --  Find a child definition with the given name.
+   --  Returns null if there is no such definition.
+   --  ------------------------------
+   function Find_Definition (From : in Definition_Type_Access;
+                             Name : in String) return Definition_Type_Access is
+      Node : Definition_Type_Access := From;
+   begin
+      while Node /= null loop
+         if Node.Name = Name then
+            return Node;
+         end if;
+         Node := Node.Next;
+      end loop;
+      return null;
+   end Find_Definition;
+
+end Helios.Schemas;
