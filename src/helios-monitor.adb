e76da4cc6c968e7d65bc60a9bd2d5678b71b3ec9
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-14 18:14:04 +0200

    Refactor the Monitor package

--- src/helios-monitor.adb
+++ src/helios-monitor.adb
@@ -18,80 +18,22 @@
 
 package body Helios.Monitor is
 
-   Root          : aliased Definition_Type (Len => 0);
-   Current_Index : Value_Index := 0;
-
-   --  ------------------------------
-   --  Get the root definition tree.
-   --  ------------------------------
-   function Get_Root return Definition_Type_Access is
-   begin
-      return Root'Access;
-   end Get_Root;
-
-   --  ------------------------------
-   --  Initialize the snapshot.
-   --  ------------------------------
-   procedure Initialize (Data : in out Snapshot_Type) is
-   begin
-      Data.Values := new Value_Array (1 .. Current_Index);
-   end Initialize;
-
-   --  ------------------------------
-   --  Set the value in the snapshot.
-   --  ------------------------------
-   procedure Set_Value (Into  : in out Snapshot_Type;
-                        Def   : in Definition_Type_Access;
-                        Value : in Uint64) is
-   begin
-      if Def /= null and then Def.Index > 0 then
-         Into.Values (Def.Index) := Value;
-      end if;
-   end Set_Value;
-
    --  ------------------------------
    --  Create a new definition with the given name.
    --  ------------------------------
    function Create_Definition (Agent : in Agent_Type;
-                               Name  : in String) return Definition_Type_Access is
-      Result : constant Definition_Type_Access := new Definition_Type (Len => Name'Length);
+                               Name  : in String) return Schemas.Definition_Type_Access is
    begin
-      Current_Index := Current_Index + 1;
-      Result.Name   := Name;
-      Result.Parent := Agent.Node;
-      Result.Next   := Agent.Node.Child;
-      Agent.Node.Child := Result;
-      Result.Index  := Current_Index;
-      return Result;
-   end Create_Definition;
-
-   --  ------------------------------
-   --  Add a new definition node to the definition.
-   --  ------------------------------
-   function Create_Definition (Into : in Definition_Type_Access;
-                               Name : in String) return Definition_Type_Access is
-      Result : constant Definition_Type_Access := new Definition_Type (Len => Name'Length);
-   begin
-      Current_Index := Current_Index + 1;
-      Result.Name   := Name;
-      Result.Parent := Into;
-      Result.Next   := Into.Child;
-      Into.Child    := Result;
-      Result.Index  := Current_Index;
-      return Result;
+      return Schemas.Create_Definition (Agent.Node, Name);
    end Create_Definition;
 
    --  ------------------------------
    --  Add a definition to the agent.
    --  ------------------------------
    procedure Add_Definition (Agent : in out Agent_Type;
-                             Def   : in Definition_Type_Access) is
+                             Def   : in Schemas.Definition_Type_Access) is
    begin
-      Current_Index := Current_Index + 1;
-      Def.Parent := Agent.Node;
-      Def.Next := Agent.Node.Child;
-      Def.Index := Current_Index;
-      Agent.Node.Child := Def;
+      Schemas.Add_Definition (Agent.Node, Def);
    end Add_Definition;
 
    --  ------------------------------
@@ -99,20 +41,14 @@ package body Helios.Monitor is
    --  Returns null if there is no such definition.
    --  ------------------------------
    function Find_Definition (Agent : in Agent_Type;
-                             Name  : in String) return Definition_Type_Access is
-      Node : Definition_Type_Access := Agent.Node;
+                             Name  : in String) return Schemas.Definition_Type_Access is
+      use type Schemas.Definition_Type_Access;
    begin
-      if Node = null then
+      if Agent.Node = null then
          return null;
+      else
+         return Schemas.Find_Definition (Agent.Node, Name);
       end if;
-      Node := Node.Child;
-      while Node /= null loop
-         if Node.Name = Name then
-            return Node;
-         end if;
-         Node := Node.Next;
-      end loop;
-      return null;
    end Find_Definition;
 
    --  ------------------------------
@@ -120,13 +56,8 @@ package body Helios.Monitor is
    --  ------------------------------
    procedure Register (Agent : in out Agent_Type'Class;
                        Name  : in String) is
-      Node : constant Definition_Type_Access := new Definition_Type (Len => Name'Length);
    begin
-      Node.Name   := Name;
-      Node.Parent := Root'Access;
-      Node.Next   := Root.Child;
-      Root.Child  := Node;
-      Agent.Node  := Node;
+      Agent.Node := Schemas.Create_Definition (null, Name, Schemas.V_NONE);
       Agent.Start;
    end Register;
 
