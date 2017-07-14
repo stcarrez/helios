dac671766a7aa745ac22d67add2a3e07367fa45f
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-07-14 20:10:36 +0200

    New package for the definition of helios commands

---
+++ src/helios-commands.ads
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------
+--  helios-commands -- Helios commands
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Commands.Drivers;
+with Util.Commands.Consoles;
+with Util.Commands.Consoles.Text;
+package Helios.Commands is
+
+   subtype Argument_List is Util.Commands.Argument_List;
+
+   type Context_Type is limited null record;
+
+   package Drivers is
+     new Util.Commands.Drivers (Context_Type => Context_Type,
+                                Driver_Name  => "helios");
+
+   Driver : Drivers.Driver_Type;
+
+   --  Initialize the commands.
+   procedure Initialize;
+
+end Helios.Commands;
