0f990f27f9801fe736f56f7e766c2035237ce4c8
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-08-02 22:34:53 +0200

    New package for the agent command

---
+++ src/helios-commands-agent.ads
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------
+--  helios-commands-agent -- Helios agent commands
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package Helios.Commands.Agent is
+
+   type Command_Type is new Helios.Commands.Drivers.Command_Type with null record;
+
+   --  Execute a information command to report information about the agent and monitoring.
+   overriding
+   procedure Execute (Command   : in Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in Command_Type;
+                   Context   : in out Context_Type);
+
+end Helios.Commands.Agent;
