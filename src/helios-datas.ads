2e0e03854a56f8a7fa2d9869aece20e2ae20ea2d
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-08-02 14:56:03 +0200

    Declare the Initialize procedure to initialize the snapshot queue Declare the Prepare procedure Update the snapshot data types

--- src/helios-datas.ads
+++ src/helios-datas.ads
@@ -31,7 +31,15 @@ package Helios.Datas is
    type Value_Array is array (Value_Array_Index range <>) of Uint64;
    type Value_Array_Access is access all Value_Array;
 
+   type Snapshot_Type;
+   type Snapshot_Type_Access is access all Snapshot_Type;
+
+   type Snapshot_Index is new Natural;
+   type Report_Index is new Natural;
+
    type Snapshot_Type is tagged limited record
+      Schema        : Helios.Schemas.Definition_Type_Access;
+      Next          : Snapshot_Type_Access;
       Time          : Ada.Calendar.Time;
       Start_Time    : Ada.Real_Time.Time;
       End_Time      : Ada.Real_Time.Time;
@@ -39,21 +47,39 @@ package Helios.Datas is
       Values        : Value_Array_Access;
    end record;
 
-   type Snapshot_Array is array (Positive range <>) of Snapshot_Type;
+   type Snapshot_Array is array (Positive range <>) of aliased Snapshot_Type;
 
-   type Snapshot_Queue_Type (Max_Count : Positive) is limited record
-      Count   : Natural := 0;
-      Schema  : Helios.Schemas.Definition_Type_Access;
-      Data    : Snapshot_Array (1 .. Max_Count);
+   type Snapshot_Queue_Type is limited record
+      Count     : Natural := 0;
+      Read_Pos  : Natural := 0;
+      Write_Pos : Natural := 0;
+      Schema    : Helios.Schemas.Definition_Type_Access;
+      --  Data      : Snapshot_Array (1 .. Max_Count);
+      First     : Snapshot_Type_Access;
+      Current   : Snapshot_Type_Access;
    end record;
    type Snapshot_Queue_Access is access all Snapshot_Queue_Type;
 
-   --  Initialize the snapshot.
-   procedure Initialize (Data : in out Snapshot_Type);
+   --  Initialize the snapshot queue for the schema.
+   procedure Initialize (Queue  : in out Snapshot_Queue_Type;
+                         Schema : in Helios.Schemas.Definition_Type_Access;
+                         Count  : in Positive);
 
    --  Set the value in the snapshot.
    procedure Set_Value (Into  : in out Snapshot_Type;
                         Def   : in Schemas.Definition_Type_Access;
                         Value : in Uint64);
+
+   --  Prepare the snapshot queue to collect new values.
+   procedure Prepare (Queue    : in out Snapshot_Queue_Type;
+                      Snapshot : out Snapshot_Type_Access);
+
+   type Snapshot_Report_Type is limited record
+      S : Natural;
+   end record;
+
+   type Report_Queue_Type is limited record
+      Snapshot : Snapshot_Queue_Access;
+   end record;
 
 end Helios.Datas;
