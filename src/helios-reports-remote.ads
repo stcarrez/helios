c8719c6c87a4dc5eb3a27cccbabdae78c89e1308
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-09-09 23:43:29 +0200

    Declare Set_Server_Config procedure Add server configuration to the remote report

--- src/helios-reports-remote.ads
+++ src/helios-reports-remote.ads
@@ -20,6 +20,8 @@ with Ada.Real_Time;
 with Ada.Containers.Synchronized_Queue_Interfaces;
 with Ada.Containers.Unbounded_Synchronized_Queues;
 with Util.Events.Timers;
+with Util.Properties;
+with Swagger.Credentials.OAuth;
 package Helios.Reports.Remote is
 
    package Queue_Interface is
@@ -41,6 +43,12 @@ package Helios.Reports.Remote is
       Period   : Ada.Real_Time.Time_Span := Ada.Real_Time.Time_Span_Zero;
       Reporter : access Report_Task_Type;
       Queue    : Snapshot_Queue.Queue;
+      Host_Id  : Integer := 0;
+      Host_Key : Ada.Strings.Unbounded.Unbounded_String;
+      Username : Ada.Strings.Unbounded.Unbounded_String;
+      Password : Ada.Strings.Unbounded.Unbounded_String;
+      Scope    : Ada.Strings.Unbounded.Unbounded_String;
+      Cred     : aliased Swagger.Credentials.OAuth.OAuth2_Credential_Type;
    end record;
 
    procedure Start (Report : in Remote_Report_Access);
@@ -53,5 +61,9 @@ package Helios.Reports.Remote is
    --  Send a snapshot report to the server.
    procedure Send (Report : in out Remote_Report_Type;
                    Data   : in Helios.Datas.Report_Queue_Type);
+
+   --  Set the remote report configuration to connect to the server.
+   procedure Set_Server_Config (Report : in out Remote_Report_Type;
+                                Config : in Util.Properties.Manager);
 
 end Helios.Reports.Remote;
